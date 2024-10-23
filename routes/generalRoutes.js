9b0c8622be7f73c06f449c72b846fb367102d8bf
Citlalli Perez Dionicio <230237@utxicotepec.edu.mx>
2024-10-23 11:48:25 -0600

    Práctica 10
    Agregación de las rutas generales y de los usuario

---
+++ routes/generalRoutes.js
@@ -0,0 +1,21 @@
+import express, { Router } from 'express';
+
+const router = express.Router();  
+
+
+router.get("/", function(req, res){
+    res.send("Hola desde la Web, en NodeJS")
+})
+
+router.get("/quienEres", function(req, res){
+    res.json(
+        {
+     "nombre": "Citlalli",
+     "carrera": "DSM",
+     "grado": "4",
+     "grupo": "A"
+       }
+    )     
+})
+
+export default router;  //?Esta palabra reservada de JS me permite exportar los elementos que estan dentro de este archivo 
